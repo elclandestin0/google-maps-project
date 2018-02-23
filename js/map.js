c7998f2cab1d03189e2d963336aa4f6be4081787
mk <majd.khoury93@gmail.com>
2018-02-23 18:30:30 -0500

    fix: removed deadcode

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@
 var map;
 var jsonData;
 var foodListings;
+var markers;
 FOURSQUARE_CLIENT_ID = "IGSBB23NYXAIMP5CO1OVV4M3DSR5PFCMDYF5UAWHSRKK4AJH";
 FOURSQUARE_CLIENT_SECRET = "H3S03FQBEVV3YCRRORCQLG4TFKQYWM00POWVXAUQZCNVWGF3";
 var foodCategoryId = '4d4b7105d754a06374d81259';
@@ -192,49 +193,10 @@ function goToArea() {
     };
 }
 
-// function queryLists(){
-//   // STEP 1: Id's of different categories, from foursquare.
-//   var foodCategoryId = '4d4b7105d754a06374d81259';
-//   var nightlifeCategoryId = '4d4b7105d754a06376d81259'
-//
-//   // Here, we create an array of category Id's to loop through and add through
-//   // our models (via the number of API requests, 2 for now). STEP 2
-//   var categoriesId = [foodCategoryId, nightlifeCategoryId];
-//   var foodData;
-//   // STEP 3: for loop for making URLs and requests for the 2 different categories
-//   var foursquareUrl = "https://api.foursquare.com/v2/venues/search";
-//   foursquareUrl += '?' + $.param({
-//     'client_id': FOURSQUARE_CLIENT_ID,
-//     'client_secret': FOURSQUARE_CLIENT_SECRET,
-//     'll': map.getCenter().lat() + ', ' + map.getCenter().lng(),
-//     'categoryId': foodCategoryId,
-//     'v':"20180101"
-//     });
-//
-//   // we store the jsonData of the food in a variable here.
-//       jsonData = (function () {
-//         jsonData = null;
-//         $.ajax({
-//             'async': false,
-//             'global': true,
-//             'url': foursquareUrl,
-//             'dataType': "json",
-//             'success': function (data) {
-//                 jsonData = data;
-//             }
-//         });
-//         return jsonData;
-//     })();
-// }
-
 var Listing = function(data) {
     this.name = ko.observable(data.name);
     this.address = ko.observable(data.address);
 }
-// {
-//   name: jsonData.response.venues[0].name,
-//   address: jsonData.response.venues[0].location.address
-// }
 
 var ViewModel = function() {
     var self = this;
